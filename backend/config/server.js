7c2c48fefa1b344cf248831664c3e4129eb15c72
Scraggy Duck <nqviet.dev@gmail.com>
2022-04-20 09:44:25 +0700

    fix: fix deploy config

--- backend/config/server.js
+++ backend/config/server.js
@@ -1,7 +1,8 @@
 module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
+  host: env("HOST", "0.0.0.0"),
+  port: env.int("PORT", 1337),
   app: {
-    keys: env.array('APP_KEYS'),
+    keys: env.array("APP_KEYS"),
   },
+  url: "http://27.71.237.228:8131",
 });
